f13db42e30f33b3d5a2a4da94a5c1c032161c148
Taran <tdesh10@gmail.com>
2024-02-02 14:14:02 -0800

    Fix search-by-tag (extra dash) in eip

--- examples/cleu-topology/infra/main.tf
+++ examples/cleu-topology/infra/main.tf
@@ -248,14 +248,14 @@ data "aws_region" "current" {}
 data "aws_eip" "alpha_eip" {
   filter {
     name   = "tag:Name"
-    values = ["${local.name_prefix}-${data.aws_region.current.name}-a"]
+    values = ["${local.name_prefix}-${data.aws_region.current.name}a"]
   }
 }
 
 data "aws_eip" "beta_eip" {
   filter {
     name   = "tag:Name"
-    values = ["${local.name_prefix}-${data.aws_region.current.name}-b"]
+    values = ["${local.name_prefix}-${data.aws_region.current.name}b"]
   }
 }
 
