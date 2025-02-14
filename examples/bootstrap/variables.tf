43c3010cdd4bad5148631f23ad5bc7ad5f51248a
Taran <33609321+tdesh10@users.noreply.github.com>
2025-02-14 12:10:56 +0100

    Update variables.tf

--- examples/bootstrap/variables.tf
+++ examples/bootstrap/variables.tf
@@ -10,6 +10,6 @@ variable "name_prefix" {
 variable "cluster_version" {
   description = "Cluster version"
   type        = string
-  default     = "1.31"
+  default     = "1.32"
   nullable    = false
 }
